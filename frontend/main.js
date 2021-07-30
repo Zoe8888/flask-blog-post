31e79acc6569105a3a9ad01305d3f6edbd572756
Zoe8888 <zoeerispe@gmail.com>
2021-07-30 10:47:52 +0200

    js sorted

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,3 @@
-fetch('https://warm-ocean-75768.herokuapp.com/')
+fetch('https://warm-ocean-75768.herokuapp.com/get-blogs/')
 .then(response => response.json())
-.then(data)
-console.log(data)
+.then(data => console.log(data))
