96a7cdb4b433f64661cf39a4e0213cd83d565164
Zoe8888 <zoeerispe@gmail.com>
2021-07-30 14:33:32 +0200

    css added

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,13 @@
 fetch('https://warm-ocean-75768.herokuapp.com/get-blogs/')
 .then(response => response.json())
 .then(data => console.log(data))
+
+const token = 'YOUR_TOKEN_HERE';
+fetch('https://warm-ocean-75768.herokuapp.com/auth', {
+    method: 'post',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify({"username":"zozo", "password":"ronbon"})
+}).then(res => res.json())
+.then(res => console.log(res));
